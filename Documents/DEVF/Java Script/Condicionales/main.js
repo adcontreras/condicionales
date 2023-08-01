907567303da4112ca9f51de4e76e8a7dabb48397
adcontreras <adcontrerasmm@gmail.com>
2023-07-31 23:26:03 -0600

    ejercicio 4 1000-ganaste un premio

--- Documents/DEVF/Java Script/Condicionales/main.js
+++ Documents/DEVF/Java Script/Condicionales/main.js
@@ -21,18 +21,29 @@
 
 // 3. Crear un programa que determine si un número introducido en un Prompt es par o no, la respuesta será mostrada en un Alert.
 
-let num = parseInt(prompt('Ingresa un número'));
+// let num = parseInt(prompt('Ingresa un número'));
 
-if(num % 2 === 0){
-    console.log(`${num} es número par`);
-    alert(`${num} es número par`);
+// if(num % 2 === 0){
+//     console.log(`${num} es número par`);
+//     alert(`${num} es número par`);
+// }else{
+//     console.log(`${num} no es número par`);
+//     alert(`${num} no es número par`);
+// }
+
+
+// 4. Solicitar al usuario un número de cliente. Si el número es el 1000, imprimir 'Ganaste u prnemio', en caso contrario mostrar el número y el mensaje “Lo sentimos, sigue participando”.
+
+
+let num = parseInt(prompt('Ingresa un número de cliente'));
+
+if(num === 1000){
+    console.log('Felicidades ganaste un premio');
 }else{
-    console.log(`${num} no es número par`);
-    alert(`${num} no es número par`);
+    console.log(`${num} Lo sentimos, sigue participando`);
 }
 
 
-// 4. Solicitar al usuario un número de cliente. Si el número es el 1000, imprimir 'Ganaste un premio', en caso contrario mostrar el número y el mensaje “Lo sentimos, sigue participando”.
 // 5. Solicitar al usuario que ingrese dos números y mostrar cuál de los dos es menor. No considerar el caso en que ambos números son iguales.
 // 6. Solicitar al usuario que ingrese tres números y mostrar cuál de los tres es el número mayor. Considerar el caso en que 2 números sean iguales.
 // 7. Requerir al usuario que ingrese un día de la semana e imprimir un mensaje si es lunes, otro mensaje diferente si es viernes, otro mensaje diferente si es sábado o domingo. Si el día ingresado no es ninguno de esos, imprimir otro mensaje.
